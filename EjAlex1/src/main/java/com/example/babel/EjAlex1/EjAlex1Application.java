d060cfe4eb18693e0f986cdfde8190e3a4d5ae76
alvaro <alvaro_penalva@hotmail.com>
2023-06-12 11:30:25 +0200

    Primer commit

---
+++ EjAlex1/src/main/java/com/example/babel/EjAlex1/EjAlex1Application.java
@@ -0,0 +1,13 @@
+package com.example.babel.EjAlex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"controller"})
+public class EjAlex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjAlex1Application.class, args);
+	}
+
+}
---
+++ EjAlex1/src/test/java/com/example/babel/EjAlex1/EjAlex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babel.EjAlex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjAlex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
